928debb90804bac0d1bfd6c1eee09caca79e7bc1
AggieWB <usmaggie23@gmail.com>
2020-07-01 13:51:42 -0700

    Add url routes for the home page and about page
    Add contents to write a reponse for the about route

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ http.createServer((request, response) => {
     const appDescription = '<p>Here, I\'ve imported a custom Node module that contains a function that returns an array of objects.</p>'
     const objectDescription = `<p>Total items in my data array is ${data().length}.</p><p>This is the contents of the array:</p>`
     const employeesListContents = data().map(employee => `<li>${JSON.stringify(employee)}</li>`);
+    const aboutAuthor = '<h1>About me</h1><p>Let me tell you a little about myself! I am working towards a web development certificate from Seattle Central College, and I currently live in Seattle with my husband. I have experience in HTML, CSS, Python, JS, SQL, PHP, Django, and Java. In my off time, I really enjoy reading, being outside, and playing video and board games.<p>';
 
-    response.writeHead(200, {'Content-type': 'text/html'});
-    response.write(`${greeting}${appDescription}`);
-    response.write(objectDescription);
-    response.write(`<ul>${employeesListContents.join('')}</ul>`);
-    response.end();
+    const path = request.url.toLowerCase();
+
+    if(path === '/'){
+        response.writeHead(200, {'Content-type': 'text/html'});
+        response.write(`${greeting}${appDescription}`);
+        response.write(objectDescription);
+        response.write(`<ul>${employeesListContents.join('')}</ul>`);
+        response.end();
+    } else if(path === '/about'){
+        response.writeHead(200, {'Content-type': 'text/html'});
+        response.write(aboutAuthor);
+        response.end();
+    }
 }).listen(3000);
