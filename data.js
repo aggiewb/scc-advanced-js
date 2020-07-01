01b3ae4392e567717c8b2e0468f5553671915000
AggieWB <usmaggie23@gmail.com>
2020-06-30 22:19:56 -0700

    Add a function that returns an array of objects to be exported

---
+++ data.js
@@ -0,0 +1,34 @@
+const getAll = () => [
+    {
+        employee: 'Aggie',
+        title: 'Web Dev 1',
+        years: 2,
+        salary: 80000
+    }, 
+    {
+        employee: 'Steven',
+        title: 'Lead DevOps Developer',
+        years: 5,
+        salary: 150000
+    }, 
+    {
+        employee: 'Sherman',
+        title: 'Web Dev 2',
+        years: 3,
+        salary: 90000
+    }, 
+    {
+        employee: 'Evelynn',
+        title: 'Senior Developer',
+        years: 8,
+        salary: 200000
+    }, 
+    {
+        employee: 'Frank',
+        title: 'QA Engineer',
+        years: 2,
+        salary: 70000
+    }
+];
+
+module.exports = {getAll}; 
