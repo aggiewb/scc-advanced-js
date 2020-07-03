5b5ca66dd46461e6533e22d67ab3336223370a6d
AggieWB <usmaggie23@gmail.com>
2020-07-03 10:07:36 -0700

    Update syntax errors and string layout for easier readability

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const http = require('http');
 
 http.createServer((request, response) => {
     const greeting = '<h1>Hi, my name is Aggie, and this is my first Node.js app!</h1>';
-    const appDescription = '<p>Here, I\'ve imported a custom Node module that contains a function that returns an array of objects.</p>'
-    const objectDescription = `<p>Total items in my data array is ${data().length}.</p><p>This is the contents of the array:</p>`
+    const appDescription = '<p>Here, I\'ve imported a custom Node module that contains a function that returns an array of objects.</p>';
+    const objectDescription = `<p>Total items in my data array is ${data().length}.</p><p>This is the contents of the array:</p>`;
     const employeesListContents = data().map(employee => `<li>${JSON.stringify(employee)}</li>`);
-    const aboutAuthor = '<h1>About me</h1><p>Let me tell you a little about myself! I am working towards a web development certificate from Seattle Central College, and I currently live in Seattle with my husband. I have experience in HTML, CSS, Python, JS, SQL, PHP, Django, and Java. In my off time, I really enjoy reading, being outside, and playing video and board games.<p>';
+    let aboutAuthor = '<h1>About me</h1><p>Let me tell you a little about myself! ';
+    aboutAuthor += 'I am working towards a web development certificate from Seattle Central College, and I currently live in Seattle with my husband. ';
+    aboutAuthor += 'I have experience in HTML, CSS, Python, JS, SQL, PHP, Django, and Java. In my off time, ';
+    aboutAuthor += 'I really enjoy reading, being outside, and playing video/board games.<p>';
     const error404 = '<h1>404 error</h1><p>Sorry! That page couldn\'t be found.</p>';
 
     const path = request.url.toLowerCase();
