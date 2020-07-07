ed0f4b951ebf4a19e1b1de83bef298f4a960741a
AggieWB <usmaggie23@gmail.com>
2020-07-07 14:37:47 -0700

    Comment out html server to use as reference in refactor
    Add express route handlers for home, about, and 404
    Add listen method to start web server

--- index.js
+++ index.js
@@ -3,32 +3,49 @@ const data = require('./data.js');
 const express = require('express');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
+app.listen(app.get('port'));
 
-http.createServer((request, response) => {
-    const greeting = '<h1>Hi, my name is Aggie, and this is my first Node.js app!</h1>';
-    const appDescription = '<p>Here, I\'ve imported a custom Node module that contains a function that returns an array of objects.</p>';
-    const objectDescription = `<p>Total items in my data array is ${data().length}.</p><p>This is the contents of the array:</p>`;
-    const employeesListContents = data().map(employee => `<li>${JSON.stringify(employee)}</li>`);
-    let aboutAuthor = '<h1>About me</h1><p>Let me tell you a little about myself! ';
-    aboutAuthor += 'I am working towards a web development certificate from Seattle Central College, and I currently live in Seattle with my husband. ';
-    aboutAuthor += 'I have experience in HTML, CSS, Python, JS, SQL, PHP, Django, and Java. In my off time, ';
-    aboutAuthor += 'I really enjoy reading, being outside, and playing video/board games.<p>';
-    const error404 = '<h1>404 error</h1><p>Sorry! That page couldn\'t be found.</p>';
+app.use((request, response) => {
+    response.type('text/html');
+    response.status(404);
+    response.send('404 - Not found');
+})
 
-    const path = request.url.toLowerCase();
+app.get('/about', (request, response) => {
+    response.type('text/html');
+    response.send('About page');
+});
 
-    if(path === '/'){
-        response.writeHead(200, {'Content-type': 'text/html'});
-        response.write(`${greeting}${appDescription}`);
-        response.write(objectDescription);
-        response.write(`<ul>${employeesListContents.join('')}</ul>`);
-    } else if(path === '/about'){
-        response.writeHead(200, {'Content-type': 'text/html'});
-        response.write(aboutAuthor);
-    } else {
-        response.writeHead(404, {'Content-Type': 'text/html'});
-        response.write(error404);
-    }
-    response.end();
-}).listen(3000);
+app.get('/', (request, response) => {
+    response.type('text/html');
+    response.send('Home page');
+});
+
+// http.createServer((request, response) => {
+//     const greeting = '<h1>Hi, my name is Aggie, and this is my first Node.js app!</h1>';
+//     const appDescription = '<p>Here, I\'ve imported a custom Node module that contains a function that returns an array of objects.</p>';
+//     const objectDescription = `<p>Total items in my data array is ${data().length}.</p><p>This is the contents of the array:</p>`;
+//     const employeesListContents = data().map(employee => `<li>${JSON.stringify(employee)}</li>`);
+//     let aboutAuthor = '<h1>About me</h1><p>Let me tell you a little about myself! ';
+//     aboutAuthor += 'I am working towards a web development certificate from Seattle Central College, and I currently live in Seattle with my husband. ';
+//     aboutAuthor += 'I have experience in HTML, CSS, Python, JS, SQL, PHP, Django, and Java. In my off time, ';
+//     aboutAuthor += 'I really enjoy reading, being outside, and playing video/board games.<p>';
+//     const error404 = '<h1>404 error</h1><p>Sorry! That page couldn\'t be found.</p>';
+
+//     const path = request.url.toLowerCase();
+
+//     if(path === '/'){
+//         response.writeHead(200, {'Content-type': 'text/html'});
+//         response.write(`${greeting}${appDescription}`);
+//         response.write(objectDescription);
+//         response.write(`<ul>${employeesListContents.join('')}</ul>`);
+//     } else if(path === '/about'){
+//         response.writeHead(200, {'Content-type': 'text/html'});
+//         response.write(aboutAuthor);
+//     } else {
+//         response.writeHead(404, {'Content-Type': 'text/html'});
+//         response.write(error404);
+//     }
+//     response.end();
+// }).listen(3000);
