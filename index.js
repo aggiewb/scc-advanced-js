e876861d6c859b11126dfd5e325415d9301af0d7
AggieWB <usmaggie23@gmail.com>
2020-07-07 15:59:22 -0700

    Require handlebars node module, register view engine to
    handlebars, and register a template engine to map to handlebars layout
    Update the '/' route to render home.handlebars passing in an object containing an array of employee objects
    Remove commenting

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 'use strict'
-const data = require('./data.js');
 const express = require('express');
+const handlebars = require('express-handlebars');
+const data = require('./data.js');
 
 const app = express();
 app.set('port', 3000);
+app.engine('handlebars', handlebars({defaultLayout: false}));
+app.set('view engine', 'handlebars');
 app.listen(app.get('port'));
-
-app.use((request, response) => {
-    response.type('text/html');
-    response.status(404);
-    response.send('404 - Not found');
-})
 
 app.get('/about', (request, response) => {
     response.type('text/html');
@@ -18,34 +15,11 @@ app.get('/about', (request, response) => {
 });
 
 app.get('/', (request, response) => {
-    response.type('text/html');
-    response.send('Home page');
+    response.render('home', {employees: data()});
 });
 
-// http.createServer((request, response) => {
-//     const greeting = '<h1>Hi, my name is Aggie, and this is my first Node.js app!</h1>';
-//     const appDescription = '<p>Here, I\'ve imported a custom Node module that contains a function that returns an array of objects.</p>';
-//     const objectDescription = `<p>Total items in my data array is ${data().length}.</p><p>This is the contents of the array:</p>`;
-//     const employeesListContents = data().map(employee => `<li>${JSON.stringify(employee)}</li>`);
-//     let aboutAuthor = '<h1>About me</h1><p>Let me tell you a little about myself! ';
-//     aboutAuthor += 'I am working towards a web development certificate from Seattle Central College, and I currently live in Seattle with my husband. ';
-//     aboutAuthor += 'I have experience in HTML, CSS, Python, JS, SQL, PHP, Django, and Java. In my off time, ';
-//     aboutAuthor += 'I really enjoy reading, being outside, and playing video/board games.<p>';
-//     const error404 = '<h1>404 error</h1><p>Sorry! That page couldn\'t be found.</p>';
-
-//     const path = request.url.toLowerCase();
-
-//     if(path === '/'){
-//         response.writeHead(200, {'Content-type': 'text/html'});
-//         response.write(`${greeting}${appDescription}`);
-//         response.write(objectDescription);
-//         response.write(`<ul>${employeesListContents.join('')}</ul>`);
-//     } else if(path === '/about'){
-//         response.writeHead(200, {'Content-type': 'text/html'});
-//         response.write(aboutAuthor);
-//     } else {
-//         response.writeHead(404, {'Content-Type': 'text/html'});
-//         response.write(error404);
-//     }
-//     response.end();
-// }).listen(3000);
+app.use((request, response) => {
+    response.type('text/html');
+    response.status(404);
+    response.send('404 - Not found');
+});
