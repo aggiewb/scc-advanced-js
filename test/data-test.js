49530ff666989dc47dd13a9997b2426c694b523f
AggieWB <usmaggie23@gmail.com>
2020-07-15 16:03:29 -0700

    Update variable names and remove third string parameter
    dsecribing failure in some assertion methods

--- test/data-test.js
+++ test/data-test.js
@@ -3,17 +3,17 @@ const data = require('../data.js');
 
 describe('Calling data.getEmployee()', () => {
     describe('Call with a defined employee', () => {
-        it('Should return a defined employee object when called', () => {
+        it('Should return an existing employee object when called', () => {
             const response = data.getEmployee('Aggie');
-            assert.isNotNull(response.employee, 'The employee is null');
-            assert.strictEqual(response.status, 'Employee found', 'The employee does not exist');
+            assert.isNotNull(response.employee);
+            assert.strictEqual(response.status, 'Employee found');
         });
     });
     describe('Call with an undefined employee', () => {
         it('Should return undefined when called', () => {
             const response = data.getEmployee('Rob');
-            assert.isNull(response.employee, 'The employee is not null');
-            assert.strictEqual(response.status, 'Employee not found', 'The employee exists');
+            assert.isNull(response.employee);
+            assert.strictEqual(response.status, 'Employee not found');
         });
     });
 });
@@ -21,9 +21,9 @@ describe('Calling data.getEmployee()', () => {
 describe('Calling data.addEmployee()', () => {
     describe('Call with all valid parameters', () => {
         it('Should add a new employee', () => {
-            const employeeToAdd = data.addEmployee('John', 'test dev', 1, 500);
-            assert.strictEqual(employeeToAdd.status, 'Employee added');
-            assert.strictEqual(employeeToAdd.employeeSize, data.getAll().length);
+            const employeeAdded = data.addEmployee('John', 'test dev', 1, 500);
+            assert.strictEqual(employeeAdded.status, 'Employee added');
+            assert.strictEqual(employeeAdded.employeeSize, data.getAll().length);
         });
     });
     describe('Call with all valid parameters but employee already exists', () => {
