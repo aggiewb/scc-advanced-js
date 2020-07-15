580b9c541b3b052b247510aea4a3fe5f07f75a7e
AggieWB <usmaggie23@gmail.com>
2020-07-15 16:13:41 -0700

    Update describe() and it() string descriptions for better
    clarification

--- test/data-test.js
+++ test/data-test.js
@@ -2,15 +2,15 @@ const assert = require('chai').assert;
 const data = require('../data.js');
 
 describe('Calling data.getEmployee()', () => {
-    describe('Call with a defined employee', () => {
+    describe('Passing in a defined employee', () => {
         it('Should return an existing employee object when called', () => {
             const response = data.getEmployee('Aggie');
             assert.isNotNull(response.employee);
             assert.strictEqual(response.status, 'Employee found');
         });
     });
-    describe('Call with an undefined employee', () => {
-        it('Should return undefined when called', () => {
+    describe('Passing in an undefined employee', () => {
+        it('Should return null when called', () => {
             const response = data.getEmployee('Rob');
             assert.isNull(response.employee);
             assert.strictEqual(response.status, 'Employee not found');
@@ -19,19 +19,19 @@ describe('Calling data.getEmployee()', () => {
 });
 
 describe('Calling data.addEmployee()', () => {
-    describe('Call with all valid parameters', () => {
-        it('Should add a new employee', () => {
+    describe('Passing in all valid parameters', () => {
+        it('Should add a new employee, return status "Employee added" and new size of all employees', () => {
             const employeeAdded = data.addEmployee('John', 'test dev', 1, 500);
             assert.strictEqual(employeeAdded.status, 'Employee added');
             assert.strictEqual(employeeAdded.employeeSize, data.getAll().length);
         });
     });
-    describe('Call with all valid parameters but employee already exists', () => {
+    describe('Passing in all valid parameters but employee already exists', () => {
         it('Should return with status of "Employee already exists"', () => {
             assert.strictEqual(data.addEmployee('Aggie','test dev', 1, 500).status, 'Employee already exists');
         });
     });
-    describe('Call with some parameters not passed in', () => {
+    describe('Passing in some valid parameters', () => {
         it('Should return with status of "Employee not added due to undefined values passed in"', () => {
             assert.strictEqual(data.addEmployee('Rob', 'test dev').status, 'Employee not added due to undefined values passed in');
         });
@@ -39,14 +39,14 @@ describe('Calling data.addEmployee()', () => {
 });
 
 describe('Calling data.deleteEmployee()', () => {
-    describe('Call with successful deletion of existing employee', () => {
-        it('Should delete employee', () => {
+    describe('Passing in defined employee', () => {
+        it('Should delete employee, return status "Employee found and deleted" and new size of all employees', () => {
             const employeeDeleted = data.deleteEmployee('Aggie');
             assert.strictEqual(employeeDeleted.status, "Employee found and deleted");
             assert.strictEqual(employeeDeleted.employeeSize, data.getAll().length);
         });
     });
-    describe('Call with unsuccesful deletion of nonexisting employee', () => {
+    describe('Passing in undefined employee', () => {
         it('Should return with a status of "Employee not found and unable to delete"', () => {
             assert.strictEqual(data.deleteEmployee('Rob').status, "Employee not found and unable to delete");
         });
