60e5e9fcd35af93b5789d287eeceb74d4b4d83dc
AggieWB <usmaggie23@gmail.com>
2020-07-28 13:34:18 -0700

    Add TODO comments for all required assignment routes

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,12 @@ const express = require('express');
 const Employee = require('../models/Employee.js')
 const router = express.Router();
 
+//GET route for a single employee
+
+//GET route for all employees
+
+//DELETE route for deleting an employee
+
+//POST route for adding or updating an employee
+
 module.exports = router;
